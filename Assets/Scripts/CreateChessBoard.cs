72650d4bc2c0eda27df63389015c7f16d3c47013
NoahFedder01 <nfedder01@gmail.com>
2025-09-03 14:39:46 -0400

    Update existing scripts for draggable piece system
    - Modified GameManager to work with ChessBoard instead of AdvancedSpriteRenderer
    - Updated CreateChessBoard to create individual GameObjects for squares
    - Removed old sprite rendering system dependencies

--- Assets/Scripts/CreateChessBoard.cs
+++ Assets/Scripts/CreateChessBoard.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class CreateChessBoard : MonoBehaviour
 {
-
     public Color32 darkColor = new Color32(159, 83, 18, 255),
         lightColor = new Color32(255, 209, 126, 255);
 
-    
-
-    public SpriteRenderer mySpriteRenderer;
-
-    
+    [Header("Board Settings")]
+    public float squareSize = 1f;
+    public int boardLayer = 0; // Layer for board squares (behind pieces)
 
     void CreateSquare(Color32 color, Vector2 position) {
         // Create a 1x1 texture with the given color
@@ -21,31 +18,32 @@ public class CreateChessBoard : MonoBehaviour
         // Create a sprite from the texture
         Sprite sprite = Sprite.Create(texture, new Rect(0, 0, 1, 1), new Vector2(0.5f, 0.5f));
 
-        // Prepare sprite data for the AdvancedSpriteRenderer
-        Vector2 scale = new Vector2(100f, 100f);
-
-        // Add it to the renderer (no new GameObject needed)
-        AdvancedSpriteRenderer.Instance.AddSprite(sprite, position, Quaternion.identity, scale);
+        // Create a GameObject for this square
+        GameObject square = new GameObject($"Square_{position.x}_{position.y}");
+        square.transform.parent = transform;
+        square.transform.position = new Vector3(position.x, position.y, 0f);
+        
+        // Add SpriteRenderer
+        SpriteRenderer sr = square.AddComponent<SpriteRenderer>();
+        sr.sprite = sprite;
+        sr.sortingOrder = boardLayer;
+        
+        // Scale the square
+        square.transform.localScale = Vector3.one * squareSize;
     }
 
-
-    
     void RenderChessBoard() {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 bool isLightSquare = (row + col) % 2 == 1;
                 Color32 color = isLightSquare ? lightColor : darkColor;
-                Vector2 myVec = new Vector2(row - 3.5f, col - 3.5f);
+                Vector2 myVec = new Vector2(col - 3.5f, row - 3.5f);
                 CreateSquare(color, myVec);
             }
         }
     }
+
     void Start() {
         RenderChessBoard();
-    }
-
-    // Update is called once per frame
-    void Update() {
-
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,97 +3,23 @@ using TMPro;
 
 public class GameManager : MonoBehaviour {
 
-    public char[,] boardState = new char[8, 8];
     public string FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    public Sprite[] chessPieces;
-
     public TMP_Text fenText;   // assign in Inspector
     private string previousFEN = "";
-
-    void InitializeBoardState() {
-        int FENIndex = 0, boardStateIndex = 0;
-        char curr = FEN[0];
-        while (curr != ' ') {
-            if (curr == '/') {
-                FENIndex++;
-                curr = FEN[FENIndex];
-                continue;
-            }
-            if (curr > 47 && curr < 58) {
-                int n = curr - 48;
-                while (n > 0) {
-                    boardState[7 - boardStateIndex / 8, boardStateIndex % 8] = ' ';
-                    boardStateIndex++;
-                    n--;
-                }
-                FENIndex++;
-                curr = FEN[FENIndex];
-                continue;
-            }
-            boardState[7 - boardStateIndex / 8, boardStateIndex % 8] = curr;
-            FENIndex++;
-            curr = FEN[FENIndex];
-            boardStateIndex++;
+    
+    [Header("Chess System")]
+    public ChessBoard chessBoard;
+    
+    void Awake() {
+        // Find or create chess board
+        if (chessBoard == null) {
+            chessBoard = FindFirstObjectByType<ChessBoard>();
         }
-    }
-
-    void GeneratePiece(string piece, Vector3 position) {
-        Sprite myPiece = Resources.Load<Sprite>(piece);
-        Vector3 scale = new Vector3(1f, 1f, 0f);
-
-        if (AdvancedSpriteRenderer.Instance == null) {
-            Debug.Log("Instance not found");
-        }
-
-        AdvancedSpriteRenderer.Instance.AddSprite(
-            myPiece,
-            position,
-            Quaternion.identity,
-            scale
-        );
-    }
-
-    string GivePNGFile(char pType) {
-        // Debug.Log("GivePNGFile function recieved char = '" + pType + "'");
-        switch (pType) {
-            case 'p':
-                return "black-pawn";
-            case 'n':
-                return "black-knight";
-            case 'b':
-                return "black-bishop";
-            case 'r':
-                return "black-rook";
-            case 'q':
-                return "black-queen";
-            case 'k':
-                return "black-king";
-            case 'P':
-                return "white-pawn";
-            case 'N':
-                return "white-knight";
-            case 'B':
-                return "white-bishop";
-            case 'R':
-                return "white-rook";
-            case 'Q':
-                return "white-queen";
-            default:
-                return "white-king";
-        }
-    }
-
-    void RenderPieces() {
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                char pType = boardState[row, col];
-                // Debug.Log("Board state for (" + (row) + ", " + (col) + ") = " + "'" + boardState[row, col] + "'");
-                if (pType == ' ') continue;
-                string pngFile = GivePNGFile(pType);
-                Vector3 location = new Vector3(col - 3.5f, row - 3.5f, -1f);
-                // Debug.Log("Loading piece: " + pngFile + " derived from pType = '" + pType + "' at location (" + (col + 1) + ", " + (row + 1) + ")");
-                GeneratePiece(pngFile, location);
-            }
+        
+        if (chessBoard == null) {
+            // Create a chess board GameObject if none exists
+            GameObject chessBoardObj = new GameObject("ChessBoard");
+            chessBoard = chessBoardObj.AddComponent<ChessBoard>();
         }
     }
 
@@ -112,15 +38,26 @@ public class GameManager : MonoBehaviour {
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        InitializeBoardState();
-        RenderPieces();
+        // Initialize the chess board with pieces from FEN
+        if (chessBoard != null) {
+            chessBoard.InitializeFromFEN(FEN);
+        }
         UpdateFENDisplay();
     }
 
     // Update is called once per frame
     void Update() {
         if (FEN != previousFEN) {
+            // Re-initialize board if FEN changes
+            if (chessBoard != null) {
+                chessBoard.InitializeFromFEN(FEN);
+            }
             UpdateFENDisplay();
         }
+    }
+
+    // Method to update FEN when pieces are moved (can be called by ChessBoard)
+    public void UpdateFEN(string newFEN) {
+        FEN = newFEN;
     }
 }
